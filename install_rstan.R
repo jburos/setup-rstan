31cf25200248d1309557c0d9842ada712cf617f0
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-05-02 09:09:35 -0400

    do not install unless necessary

--- install_rstan.R
+++ install_rstan.R
@@ -1,4 +1,7 @@
 
-install.packages('rstan', repos = 'http://cran.rstudio.com')
-install.packages('shinystan', repos = 'http://cran.rstudio.com')
-install.packages('loo', repos = 'http://cran.rstudio.com')
+if (!require('rstan'))
+  install.packages('rstan', repos = 'http://cran.rstudio.com')
+if (!require('shinystan'))
+  install.packages('shinystan', repos = 'http://cran.rstudio.com')
+if (!require('loo'))
+  install.packages('loo', repos = 'http://cran.rstudio.com')
